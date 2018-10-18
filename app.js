8dcfeed652658c2c1f02fd819da51148897d410c
gurpreet-aika <gurpreet@aika.digital>
2018-10-18 19:27:32 +0530

    Implement auth0 in forntemd

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 var createError = require('http-errors');
-const favicon = require('serve-favicon');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const logger = require('morgan');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const flash = require('connect-flash');
 
-var indexRouter = require('./src/routes/index');
-var usersRouter = require('./src/routes/users');
+dotenv.load();
+
+// This will configure Passport to use Auth0
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback',
+    state: true
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
 
 var app = express();
 
@@ -18,10 +43,41 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: 'shhhhhhhhh',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(flash());
+
+// Handle auth failure error messages
+app.use(function(req, res, next) {
+ if (req && req.query && req.query.error) {
+   req.flash("error", req.query.error);
+ }
+ if (req && req.query && req.query.error_description) {
+   req.flash("error_description", req.query.error_description);
+ }
+ next();
+});
+
+// Check logged in
+app.use(function(req, res, next) {
+  res.locals.loggedIn = false;
+  if (req.session.passport && typeof req.session.passport.user !== 'undefined') {
+    res.locals.loggedIn = true;
+  }
+  next();
+});
+
 //setup routes
-require('./src/routes/routes')(app);
+require('./src/routes/routes')(app, passport);
 
 
 // catch 404 and forward to error handler
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,23 @@
 // Frontend Routes
-var indexRouter = require('./src/routes/index');
-var usersRouter = require('./src/routes/users');
+var indexRouter = require('./index');
+var usersRouter = require('./user');
 
-exports = module.exports = function(app) {
+//API'S
+var loginRouter = require('./login');
+exports = module.exports = function(app, passport) {
   	//front end
   	app.use('/', indexRouter);
 	app.use('/users', usersRouter);
+
+	//API'S
+	app.use('/login', loginRouter);
+
+	// you can use this section to keep a smaller payload
+	passport.serializeUser(function(user, done) {
+	  done(null, usersRouter);
+	});
+
+	passport.deserializeUser(function(user, done) {
+	  done(null, usersRouter);
+	});
 };
