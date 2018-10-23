a9e679ef3c7f75edd851c0b2db61cdd818ee8299
gurpreet-aika <gurpreet@aika.digital>
2018-10-23 19:28:08 +0530

    Create an login with aouth0

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-const logger = require('morgan');
+const express = require('express');
+const path = require('path');
 const favicon = require('serve-favicon');
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
@@ -33,15 +32,26 @@ const strategy = new Auth0Strategy(
 
 passport.use(strategy);
 
-var app = express();
+// you can use this section to keep a smaller payload
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'pug');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   session({
@@ -77,23 +87,37 @@ app.use(function(req, res, next) {
 });
 
 //setup routes
-require('./src/routes/routes')(app, passport);
-
+require('./src/routes/routes')(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 module.exports = app;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,23 +1,7 @@
-// Frontend Routes
-var indexRouter = require('./index');
-var usersRouter = require('./user');
+const routes = require('./index');
+const user = require('./user');
 
-//API'S
-var loginRouter = require('./login');
-exports = module.exports = function(app, passport) {
-  	//front end
-  	app.use('/', indexRouter);
-	app.use('/users', usersRouter);
-
-	//API'S
-	app.use('/login', loginRouter);
-
-	// you can use this section to keep a smaller payload
-	passport.serializeUser(function(user, done) {
-	  done(null, usersRouter);
-	});
-
-	passport.deserializeUser(function(user, done) {
-	  done(null, usersRouter);
-	});
+exports = module.exports = function(app) {
+  	app.use('/', routes);
+	app.use('/user', user);
 };
